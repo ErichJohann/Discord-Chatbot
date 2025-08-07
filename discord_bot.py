7fbe00eff65e07f75584de9a0e3b9fffe088d6ae
ErichJohann <97558441+ErichJohann@users.noreply.github.com>
2025-08-07 11:43:53 -0300

    Modularizando no formato pedido

---
+++ discord_bot.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+from llm_client import getResponse
+
+load_dotenv()
+discTk = os.getenv('DISCORD_TOKEN')
+
+#handler = logging.FileHandler(filename='rimuru.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+#rimuru = discord.Client(intents=intents)
+rimuru = commands.Bot(command_prefix='--', intents=intents)
+
+@rimuru.event
+async def on_ready():
+    print(f'bot on air -- {rimuru.user.name}')
+
+@rimuru.event
+async def on_message(message):
+    if message.author == rimuru.user:
+        return
+    async with message.channel.typing():
+        response = await getResponse(message.content, message.channel.id, message.author.display_name)
+        await message.reply(response)
+
+    await rimuru.process_commands(message)
+
+@rimuru.command()
+async def slime(ctx):
+    await ctx.send("Hello! I'm rimuru the slime, a discord bot created by König Adler.")
+    await ctx.send("I am just a prototype in development used to test features and mechanics...")
+
+rimuru.run(discTk)#, log_handler=handler, log_level=logging.DEBUG)
--- llm_client.py
+++ llm_client.py
@@ -1,26 +1,11 @@
-import discord
-from discord.ext import commands
-import logging
 from groq import Groq
 from dotenv import load_dotenv
 import os
 from collections import deque
 
 load_dotenv()
-discTk = os.getenv('DISCORD_TOKEN')
 groqKey = os.getenv('GROQ_KEY')
-
 groqClient = Groq(api_key=groqKey)
-
-#handler = logging.FileHandler(filename='rimuru.log', encoding='utf-8', mode='w')
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-#rimuru = discord.Client(intents=intents)
-rimuru = commands.Bot(command_prefix='--', intents=intents)
-
-history = {}
-MAX_HIST = 5
 
 sysPrompt = """Você um assistente virtual, um bot de discord com a personalidade de Rimuru Tempest do anime 
                 Tensei Shitara Slime. Suas respostas devem ser exatamente como Rimuru responderia e você deve agir exatamente 
@@ -28,16 +13,13 @@ sysPrompt = """Você um assistente virtual, um bot de discord com a personalidad
                 de system como parte da conversa e nem retorná-las para o user. Você deve preferir gerar respostas concisas, curtas e simples
                 a menos que a situação demande o oposto"""
 
-@rimuru.event
-async def on_ready():
-    print(f'bot on air -- {rimuru.user.name}')
 
+history = {}
+MAX_HIST = 10
 def getHistory(channel):
     if channel not in history:
         history[channel] = deque(maxlen=MAX_HIST)
     return history[channel]
-
-
 
 async def getResponse(input,channel, username):
     history = getHistory(channel)
@@ -54,7 +36,6 @@ async def getResponse(input,channel, username):
             "content": msg["content"]
         })
         #print(msg['content'])
-
     messages.append({"role": "user", "content": f'{username}: {input}'}) 
     
     try:
@@ -69,23 +50,6 @@ async def getResponse(input,channel, username):
         history.append({'content': completion.choices[0].message.content, 'is_bot': True})
 
         return completion.choices[0].message.content
+    
     except Exception:
         return "Não consigo pensar direito, estou confuso... Acho que meu mana acabou\n Zzz..."
-
-
-@rimuru.event
-async def on_message(message):
-    if message.author == rimuru.user:
-        return
-    async with message.channel.typing():
-        response = await getResponse(message.content, message.channel.id, message.author.display_name)
-        await message.reply(response)
-
-    await rimuru.process_commands(message)
-
-@rimuru.command()
-async def slime(ctx):
-    await ctx.send("Hello! I'm rimuru the slime, a discord bot created by König Adler.")
-    await ctx.send("I am just a prototype in development used to test features and mechanics...")
-
-rimuru.run(discTk)#, log_handler=handler, log_level=logging.DEBUG)
